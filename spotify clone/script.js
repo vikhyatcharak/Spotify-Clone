b6c285e6d5ed810be821dc5a23777203612baf1f
Vikhyat Charak <155523521+vikhyatcharak@users.noreply.github.com>
2025-01-24 18:14:00 +0530

    Update script.js

--- spotify clone/script.js
+++ spotify clone/script.js
@@ -16,7 +16,7 @@ async function getFolders() {
         data.forEach(async (folderData) => {
             if (folderData.type === 'dir') {
                 let folderName = folderData.name;
-                let getInfo = await fetch(`https://raw.githubusercontent.com/vikhyatcharak/Spotify-Clone/main/spotify%20clone/songs/${folderName}/info.json`);
+                let getInfo = await fetch(`https://api.github.com/repos/vikhyatcharak/Spotify-Clone/contents/spotify%20clone/songs/${folderName}/info.json`);
                 
                 if (!getInfo.ok) {
                     throw new Error(`Failed to fetch info.json for folder ${folderName}`);
@@ -30,7 +30,7 @@ async function getFolders() {
                                                         <path d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z" stroke="black" stroke-width="1.5" stroke-linejoin="round" />
                                                     </svg>
                                                 </button>
-                                                <img src="https://raw.githubusercontent.com/vikhyatcharak/Spotify-Clone/main/spotify%20clone/songs/${folderName}/cover.jpg" alt="${folderName}" style="filter: invert(0);">
+                                                <img src="https://api.github.com/repos/vikhyatcharak/Spotify-Clone/contents/spotify%20clone/songs/${folderName}/cover.jpg" alt="${folderName}" style="filter: invert(0);">
                                                 <h2>${r.title}</h2>
                                                 <p>${r.description}</p>
                                             </div>`;
@@ -80,7 +80,7 @@ function getCircularElement(arr, index) {
 }
 
 const playMusic = (track, pause = false) => { //pause=false because whenever we open the website then 1st song in library always in playBar
-    currSong.src = `https://raw.githubusercontent.com/vikhyatcharak/Spotify-Clone/main/spotify%20clone/songs/${currFolder}/` + track + `.mp3`;
+    currSong.src = `https://api.github.com/repos/vikhyatcharak/Spotify-Clone/contents/spotify%20clone/songs/${currFolder}/` + track + `.mp3`;
     if (!pause) {
         currSong.play();
         document.getElementById("pl").src = "material/pause.svg";
@@ -121,15 +121,15 @@ async function updateSongs(folder) {
 function playPreviousSong() {
     console.log(currSong)
     const src = currSong.src; 
-    const songName = src.split(`https://raw.githubusercontent.com/vikhyatcharak/Spotify-Clone/main/spotify%20clone/songs/${currFolder}/`)[1].split(".mp3")[0]; // Extract the song name from the URL
+    const songName = src.split(`https://api.github.com/repos/vikhyatcharak/Spotify-Clone/contents/spotify%20clone/songs/${currFolder}/`)[1].split(".mp3")[0]; // Extract the song name from the URL
     const prevSong = getCircularElement(songs, songs.indexOf(songName) - 1); 
     playMusic(prevSong);
 }
 
 function playNextSong() {
-    console.log(currSong.)
+    console.log(currSong)
     const src = currSong.src; 
-    const songName = src.split(`https://raw.githubusercontent.com/vikhyatcharak/Spotify-Clone/main/spotify%20clone/songs/${currFolder}/`)[1].split(".mp3")[0]; // Extract the song name from the URL
+    const songName = src.split(`https://api.github.com/repos/vikhyatcharak/Spotify-Clone/contents/spotify%20clone/songs/${currFolder}/`)[1].split(".mp3")[0]; // Extract the song name from the URL
     const nextSong = getCircularElement(songs, songs.indexOf(songName) + 1); 
     playMusic(nextSong);
 }
@@ -235,6 +235,7 @@ async function main() {
         e.querySelector(".green-button").addEventListener("click", async ()=>{
             // console.log(e.querySelector("h2").innerHTML)
             currFolder=e.querySelector("h2").innerHTML
+            console.log(currFolder)
             await updateSongs(currFolder)
         })
     })
