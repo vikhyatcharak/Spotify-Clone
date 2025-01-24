454a2e96ea8546fed16dda80c8c89deb6384f455
Vikhyat Charak <155523521+vikhyatcharak@users.noreply.github.com>
2025-01-24 18:52:41 +0530

    Update script.js

--- spotify clone/script.js
+++ spotify clone/script.js
@@ -2,7 +2,7 @@ let currSong = new Audio();
 let currFolder = "og playlist";
 let songs = [];
 
-const token = "your-github-token"; // Replace with your GitHub token
+const token = "Spotify Clone API Token"; // Replace with your GitHub token
 
 // Add Authorization header to your fetch requests
 const fetchWithAuth = async (url) => {
